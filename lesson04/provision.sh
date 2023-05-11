c871a4e20d4a69244c3791553091e126dd1dc993
Dodexq <dodexpwnz@gmail.com>
2023-05-11 23:24:42 +0300

    master-slave

---
+++ lesson04/provision.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt upgrade
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+apt update
+apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl status mongod
