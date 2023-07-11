9fe1d3ad367c1dfb7504c097683808a154bdb2ad
Dodexq <dodexpwnz@gmail.com>
2023-07-11 10:37:51 +0300

    in working

--- lesson21/qneo4j.py
+++ lesson21/qneo4j.py
@@ -1,10 +1,5 @@
+import random
 from neo4j import GraphDatabase
-
-touroperators = ["Пегас Туристик"]
-pegas_country = ["Абхазия"]
-abh_city = ["Гагра", "Гудаута", "Пицунда", "Цандрипш", "Новый Афон"]
-ga_city_air = ["Бабушара", "Гали", "Бабушара", "Дранда", "Зугдиди"]
-ga_city_train = ["Бабушара", "Дранда", "Зугдиди"]
 
 driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "neo4jtest"))
 
@@ -34,57 +29,66 @@ def print_tour(tx, name):
         print(record[0].id)
         print(record[0]._properties['name'])
 
-def test(tx, city, air_to):
+def test(tx, city, air_to, transport):
     cid = tx.run("MATCH (a:City {name: $city}) RETURN a", city=city).value()
     aid = tx.run("MATCH (a:City {name: $air_to}) RETURN a", air_to=air_to).value()
+    
+    try:
+        if cid[0]._properties['name'] == city and aid[0]._properties['name'] == air_to:
+            tx.run("MATCH (city:City {name: $city}) WHERE id(city) = $cid "
+                "MATCH (air_to:City {name: $air_to} WHERE id(air_to) = $aid)"
+                "MERGE (city)-[:CONNECT_BY {transport: $transport}]->(air_to)",
+                 city=city, cid=cid[0].id, air_to=air_to, aid=aid[0].id, transport=transport)
+    except IndexError:
+        pass
+
     try:
         if cid[0]._properties['name'] == city:
             tx.run("MATCH (city:City {name: $city}) WHERE id(city) = $cid "
-                "MERGE (:City {name: $city})-[:CONNECT_BY {transport: 'Airplane'}]->(at)",
-                 city=city, cid=cid[0].id, air_to=air_to)
-            
+                "MERGE (city)-[:CONNECT_BY {transport: $transport}]->(air_to:City {name: $air_to})",
+                 city=city, cid=cid[0].id, air_to=air_to, transport=transport)
+            return
     except IndexError:
-        print("Нет такого элемента", air_to)
-
-
-def test2(tx, city, air_to):
-    tx.run("CREATE (at:City {name: $city}) -[:CONNECT_BY]-> (:City {id: 1, name: $air_to})",
-           city=city, air_to=air_to)
+        pass        
     
+    try:
+        if aid[0]._properties['name'] == air_to:
+            tx.run("MATCH (air_to:City {name: $air_to}) WHERE id(air_to) = $aid "
+                "MERGE (:City {name: $city})-[:CONNECT_BY {transport: $transport}]->(air_to)",
+                 city=city, aid=aid[0].id, air_to=air_to, transport=transport)
+    except IndexError:
+        print("ТУТ")
+        tx.run("MERGE (:City {name: $city})-[:CONNECT_BY {transport: $transport}]->(:City {name: $air_to})",
+        city=city, air_to=air_to, transport=transport)
+
+
+
+def pegas():
     
-    # try:
-    #     if rid[0]._properties['name'] in air_to:
-    #         tx.run("MATCH (at:City {name: $air_to}) WHERE id(at) = $rid "
-    #                "MERGE (:City {name: $city})-[:CONNECT_BY {transport: 'Airplane'}]->(at)",
-    #                 city=city, rid=rid[0].id, air_to=air_to)
-    #     print("Просто коннект")
-    # except IndexError:
-    #         tx.run("MERGE (:City {name: $city})-[:CONNECT_BY {transport: 'Airplane'}]->(:City {name: $air_to})",
-    #         city=city, air_to=air_to)
-    #         print("ЕХЕПТ")
+    touroperators = ["Пегас Туристик"]
+    pegas_country = ["Абхазия"]
+    abh_city = ["Гагра", "Гудаута", "Пицунда", "Цандрипш", "Новый Афон"]
+    ga_city_air = ["Бабушара", "Дранда", "Зугдиди"]
+    ga_city_train = ["Бабушара", "Дранда", "Гали"]
 
+    with driver.session() as session:
+        
+        for x in touroperators:
+            session.write_transaction(add_touroperators, x)
+            for y in pegas_country:
+                session.write_transaction(add_rship_tour_to_dest, x, y)
+                for z in abh_city:
+                    session.write_transaction(add_rship_dest_to_city, y, z)
+        
+        for a in abh_city:
+            if ga_city_air:
+                session.write_transaction(test, a, ga_city_air.pop(random.randrange(len(ga_city_air))), "Airplane")
+            else: 
+                break
+        for b in abh_city:
+            if ga_city_train:
+                session.write_transaction(test, b, ga_city_train.pop(random.randrange(len(ga_city_train))), "Train")
+            else: 
+                break
 
-    #     print(rid)
-    #     print(rid[0].id, "YO")
-    #     tx.run("MATCH (at:City {name: $air_to}) WHERE id(at) = $rid "
-    #     "MERGE (:City {name: $city})-[:CONNECT_BY {transport: 'Airplane'}]->(at)",
-    #     city=city, rid=rid[0].id, air_to=air_to)
-    # else:
-    #     print(rid, "NOYO")
-    #     tx.run("MERGE (:City {name: $city})-[:CONNECT_BY {transport: 'Airplane'}]->(:City {name: $air_to})",
-    #     city=city, air_to=air_to)
-
-with driver.session() as session:
-    session.write_transaction(test2, "Гагра", "Веталь")
-
-
-    # for x in touroperators:
-    #     session.write_transaction(add_touroperators, x)
-    #     for y in pegas_country:
-    #         session.write_transaction(add_rship_tour_to_dest, x, y)
-    #         for z in abh_city:
-    #             session.write_transaction(add_rship_dest_to_city, y, z)
-    #             for a in ga_city_air:
-    #                 session.write_transaction(test, z, a)
-
-driver.close()
+    driver.close()
