bdb9e1884cb28d0c770284f57c5860ebe15cbd51
Dodexq <dodexpwnz@gmail.com>
2023-07-12 11:44:28 +0300

    lesson21 done

--- lesson21/README.md
+++ lesson21/README.md
@@ -1,10 +1,20 @@
 ## Домашнее задание к уроку 21
-
-### 
-
+ 
 ### Краткое содержание
 * основные юзкейсы для графовой БД;
 * архитектура Neo4J.
+
+### Задание
+* Взять 4-5 популярных туроператора.
+* Каждый туроператор должен быть представлен в виде ноды neo4j
+* Взять 10-15 направлений, в которые данные операторы предосавляют путевки.
+* Представить направления в виде связки нод: страна - конкретное место
+* Взять ближайшие к туриситческим локацимя города, в которых есть аэропорты или вокзалы и представить их в виде нод
+* Представить маршруты между городми в виде связей. Каждый маршрут должен быть охарактеризован видом транспорта, который позволяет переместиться между точками.
+* Написать запрос, который бы выводил направление (со всеми промежуточными точками), который можно осуществить только наземным транспортом.
+* Составить план запроса для задания "Использование neo4j" (с туропертором)
+* Добавить индексы для оптимизации запроса
+* Еще раз посмотреть план запроса и убедиться, что индексыпозволили оптимизировать запрос
 
 #
 
@@ -14,43 +24,47 @@ docker-compose up
 
 #
 
+1. Данные были заполнены средствами написанного скрипта `./qneo4j.py`
+
+<p align="center"> 
+<a href="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/1.png" rel="some text"><img src="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/1.png" alt="" width="500" /></a>
+</p>
+
+2. Все возможные поездки поездами между городами, куда нельзя попасть напрямую. Пример запроса: 
 ```
-CREATE (pegas:TourOperator {name: "Пегас Туристик"}) -[:TO_DESTINATION]-> (abh:Destination {name: "Абхазия"}),
-	(abh) -[:BELONGS_TO]-> (gagra:City {name: "Гагра"}),
-	(gagra) -[:CONNECTED_BY {transport: "Airplane"}]-> (babush:City {name: "Бабушара"}),
-	(gagra) -[:CONNECTED_BY {transport: "Airplane"}]-> (galy:City {name: "Гали"}),
-	(gagra) -[:CONNECTED_BY {transport: "Train"}]-> (:City {name: "Бабушара"}),
-	(gagra) -[:CONNECTED_BY {transport: "Train"}]-> (:City {name: "Дранда"}),
-	(gagra) -[:CONNECTED_BY {transport: "Train"}]-> (:City {name: "Зугдиди"}),
-	(abh) -[:BELONGS_TO]-> (gudanuta:City {name: "Гудаута"}),
-	(gudanuta) -[:CONNECTED_BY {transport: "Train"}]-> (babush),
-	(gudanuta) -[:CONNECTED_BY {transport: "Train"}]-> (galy),
-	(gudanuta) -[:CONNECTED_BY {transport: "Airplane"}]-> (:City {name: "Мокви"}),
-	(abh) -[:BELONGS_TO]-> (:City {name: "Пицунда"}),
-	(abh) -[:BELONGS_TO]-> (cand:City {name: "Цандрипш"}),
-	(cand) -[:CONNECTED_BY {transport: "Airplane"}]-> (:City {name: "Лихни"}),
-	(cand) -[:CONNECTED_BY {transport: "Train"}]-> (:City {name: "Лихни"}),
-	(cand) -[:CONNECTED_BY {transport: "Train"}]-> (:City {name: "Инкити"}),
-	(cand) -[:CONNECTED_BY {transport: "Train"}]-> (:City {name: "Пицундра"}),
-	(abh) -[:BELONGS_TO]-> (:City {name: "Новый Афон"}),
-	(pegas) -[:TO_DESTINATION]-> (egypt:Destination {name: "Египет"}),
-	(egypt) -[:BELONGS_TO]-> (dakh:City {name: "Дахаб"}),
-	(dakh) -[:CONNECTED_BY {transport: "Train"}]-> (nuvey:City {name: "Нувейба"}),
-	(dakh) -[:CONNECTED_BY {transport: "Train"}]-> (:City {name: "Таба"}),
-	(dakh) -[:CONNECTED_BY {transport: "Airplane"}]-> (:City {name: "Таба"}),
-	(dakh) -[:CONNECTED_BY {transport: "Train"}]-> (:City {name: "Акаба"}),
-	(egypt) -[:BELONGS_TO]-> (сairo:City {name: "Каир"}),
-	(сairo) -[:CONNECTED_BY {transport: "Airplane"}]-> (:City {name: "Асьют"}),
-	(сairo) -[:CONNECTED_BY {transport: "Airplane"}]-> (:City {name: "Фарафра"}),
-	(сairo) -[:CONNECTED_BY {transport: "Airplane"}]-> (:City {name: "Гирга"}),
-	(сairo) -[:CONNECTED_BY {transport: "Train"}]-> (nuvey),
-	(egypt) -[:BELONGS_TO]-> (:City {name: "Марса Алам"}),
-	(egypt) -[:BELONGS_TO]-> (:City {name: "Матрух"}),
-	(egypt) -[:BELONGS_TO]-> (nuvey),
-	(pegas) -[:TO_DESTINATION]-> (thail:Destination {name: "Таиланд"}),
- 	(thail) -[:BELONGS_TO]-> (:City {name: "Бангок"}),
- 	(thail) -[:BELONGS_TO]-> (:City {name: "Као Лак"}),
- 	(thail) -[:BELONGS_TO]-> (:City {name: "Ко Чанг"}),
- 	(thail) -[:BELONGS_TO]-> (:City {name: "Краби"}),
- 	(thail) -[:BELONGS_TO]-> (:City {name: "Паттайя"})
+PROFILE MATCH (a:City)-[b:CONNECT_BY {transport: "Train"}]-(c:City)
+RETURN a,b,c
 ```
+Возвращает:
+<p align="center"> 
+<a href="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/2.png" rel="some text"><img src="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/2.png" alt="" width="500" /></a>
+</p>
+
+#
+
+<p align="center"> 
+<a href="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/3.png" rel="some text"><img src="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/3.png" alt="" width="500" /></a>
+</p>
+
+3. Функция `print_train()` принтует все варианты
+
+<p align="center"> 
+<a href="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/4.png" rel="some text"><img src="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/4.png" alt="" width="500" /></a>
+</p>
+
+4. PROFILE запроса без интекса:
+
+<p align="center"> 
+<a href="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/5.png" rel="some text"><img src="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/5.png" alt="" width="500" /></a>
+</p>
+
+5. PROFILE запроса, где создано 2 индекса, что ускоряет поиск.
+```
+CREATE INDEX index_city FOR (n:City) ON (n.name)
+
+CREATE INDEX rel_range_index_name FOR ()-[r:CONNECT_BY]-() ON (r.transport)
+```
+
+<p align="center"> 
+<a href="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/6.png" rel="some text"><img src="https://raw.githubusercontent.com/Dodexq/otus_nosql/main/lesson21/screenshots/6.png" alt="" width="500" /></a>
+</p>
--- lesson21/qneo4j.py
+++ lesson21/qneo4j.py
@@ -23,20 +23,14 @@ def add_connect_city_air(tx, city, air_to):
            city=city, air_to=air_to)
 
 
-def print_tour(tx, name):
-    for record in tx.run("MATCH (a:City)"
-                         "RETURN a", name=name):
-        print(record[0].id)
-        print(record[0]._properties['name'])
-
-def test(tx, city, air_to, transport):
+def unique_id(tx, city, air_to, transport):
     cid = tx.run("MATCH (a:City {name: $city}) RETURN a", city=city).value()
     aid = tx.run("MATCH (a:City {name: $air_to}) RETURN a", air_to=air_to).value()
     
     try:
         if cid[0]._properties['name'] == city and aid[0]._properties['name'] == air_to:
             tx.run("MATCH (city:City {name: $city}) WHERE id(city) = $cid "
-                "MATCH (air_to:City {name: $air_to} WHERE id(air_to) = $aid)"
+                "MATCH (air_to:City {name: $air_to}) WHERE id(air_to) = $aid "
                 "MERGE (city)-[:CONNECT_BY {transport: $transport}]->(air_to)",
                  city=city, cid=cid[0].id, air_to=air_to, aid=aid[0].id, transport=transport)
     except IndexError:
@@ -57,19 +51,26 @@ def test(tx, city, air_to, transport):
                 "MERGE (:City {name: $city})-[:CONNECT_BY {transport: $transport}]->(air_to)",
                  city=city, aid=aid[0].id, air_to=air_to, transport=transport)
     except IndexError:
-        print("ТУТ")
         tx.run("MERGE (:City {name: $city})-[:CONNECT_BY {transport: $transport}]->(:City {name: $air_to})",
         city=city, air_to=air_to, transport=transport)
 
-
+def print_tour(tx, name):
+    for record in tx.run("MATCH (a:City)"
+                         "RETURN a", name=name):
+        print(record[0].id)
+        print(record[0]._properties['name'])
 
 def pegas():
     
     touroperators = ["Пегас Туристик"]
-    pegas_country = ["Абхазия"]
+    pegas_country = ["Абхазия", "Египет"]
     abh_city = ["Гагра", "Гудаута", "Пицунда", "Цандрипш", "Новый Афон"]
-    ga_city_air = ["Бабушара", "Дранда", "Зугдиди"]
-    ga_city_train = ["Бабушара", "Дранда", "Гали"]
+    abh_city_air = ["Дранда", "Зугдиди", "Сухуми"]
+    abh_city_train = ["Бабушара", "Дранда", "Гали", "Очамчира"]
+    egypt_city = ["Дахаб", "Каир", "Марса Алам", "Матрух", "Нувейба"]
+    egypt_city_air = ["Порт-Судан", "Асьют", "Сохаг", "Халаиб"]
+    egypt_city_train = ["Кена", "Луксор", "Асуан", "Эдфу", "Халаиб", "Порт-Судан"]
+    
 
     with driver.session() as session:
         
@@ -77,18 +78,153 @@ def pegas():
             session.write_transaction(add_touroperators, x)
             for y in pegas_country:
                 session.write_transaction(add_rship_tour_to_dest, x, y)
-                for z in abh_city:
-                    session.write_transaction(add_rship_dest_to_city, y, z)
+                if y == "Абхазия":
+                    for z in abh_city:
+                        session.write_transaction(add_rship_dest_to_city, y, z)
+                elif y == "Египет":
+                    for a in egypt_city:
+                        session.write_transaction(add_rship_dest_to_city, y, a)
         
         for a in abh_city:
-            if ga_city_air:
-                session.write_transaction(test, a, ga_city_air.pop(random.randrange(len(ga_city_air))), "Airplane")
+            if abh_city_air:
+                session.write_transaction(unique_id, a, abh_city_air.pop(random.randrange(len(abh_city_air))), "Airplane")
             else: 
                 break
         for b in abh_city:
-            if ga_city_train:
-                session.write_transaction(test, b, ga_city_train.pop(random.randrange(len(ga_city_train))), "Train")
+            if abh_city_train:
+                session.write_transaction(unique_id, b, abh_city_train.pop(random.randrange(len(abh_city_train))), "Train")
+            else: 
+                break
+
+        for c in egypt_city:
+            if egypt_city_air:
+                session.write_transaction(unique_id, c, egypt_city_air.pop(random.randrange(len(egypt_city_air))), "Airplane")
+            else: 
+                break
+        for d in egypt_city:
+            if egypt_city_train:
+                session.write_transaction(unique_id, d, egypt_city_train.pop(random.randrange(len(egypt_city_train))), "Train")
             else: 
                 break
 
     driver.close()
+
+def biblio():
+    
+    touroperators = ["Библио Глобус"]
+    biblio_globus = ["Турция", "Таиланд"]
+    turk_city = ["Алания", "Анталия", "Белек", "Бодрум", "Болу"]
+    turk_city_air = ["Газипаша", "Мерси", "Конья", "Анкара", "Махмутлар"]
+    turk_city_train = ["Каргыджак", "Музкент", "Махмутлар", "Газипаша", "Ушак"]
+    tay_city = ["Бангкок", "Као Лак", "Ко Чанг", "Краби", "Пхукет"]
+    tay_city_air = ["Мэхонгсон", "Нан", "Нонтхабури", "Паккрет", "Ройет"]
+    tay_city_train = ["Сена", "Хуахин", "Банбынг", "Ройет", "Патонг", "Нан"]
+    
+
+    with driver.session() as session:
+        
+        for x in touroperators:
+            session.write_transaction(add_touroperators, x)
+            for y in biblio_globus:
+                session.write_transaction(add_rship_tour_to_dest, x, y)
+                if y == "Турция":
+                    for z in turk_city:
+                        session.write_transaction(add_rship_dest_to_city, y, z)
+                elif y == "Таиланд":
+                    for a in tay_city:
+                        session.write_transaction(add_rship_dest_to_city, y, a)
+        
+        for a in turk_city:
+            if turk_city_air:
+                session.write_transaction(unique_id, a, turk_city_air.pop(random.randrange(len(turk_city_air))), "Airplane")
+            else: 
+                break
+        for b in turk_city:
+            if turk_city_train:
+                session.write_transaction(unique_id, b, turk_city_train.pop(random.randrange(len(turk_city_train))), "Train")
+            else: 
+                break
+
+        for c in tay_city:
+            if tay_city_air:
+                session.write_transaction(unique_id, c, tay_city_air.pop(random.randrange(len(tay_city_air))), "Airplane")
+            else: 
+                break
+        for d in tay_city:
+            if tay_city_train:
+                session.write_transaction(unique_id, d, tay_city_train.pop(random.randrange(len(tay_city_train))), "Train")
+            else: 
+                break
+
+    driver.close()
+
+def coral():
+    
+    touroperators = ["Корал Тревел"]
+    coral_trevel = ["Шри-Ланка", "Азербайджан"]
+    shr_city = ["Бентота", "Галле", "Калутара", "Канди", "Коггала"]
+    shr_city_air = ["Сигирия", "Унаватуна", "Негомбо", "Полоннарува", "Калмунай"]
+    shr_city_train = ["Моратува", "Калмунай", "Джафна", "Ратнапура", "Бадулла", "Сигирия"]
+    azer_city = ["Баку", "Габала", "Нафталан", "Шахдаг", "Шеки"]
+    azer_city_air = ["Бабек", "Ахсу", "Гах", "Зердаб", "Шамкир"]
+    azer_city_train = ["Лачын", "Бабек", "Барда", "Шамкир", "Шуша", "Ярдымлы"]
+    
+
+    with driver.session() as session:
+        
+        for x in touroperators:
+            session.write_transaction(add_touroperators, x)
+            for y in coral_trevel:
+                session.write_transaction(add_rship_tour_to_dest, x, y)
+                if y == "Шри-Ланка":
+                    for z in shr_city:
+                        session.write_transaction(add_rship_dest_to_city, y, z)
+                elif y == "Азербайджан":
+                    for a in azer_city:
+                        session.write_transaction(add_rship_dest_to_city, y, a)
+        
+        for a in shr_city:
+            if shr_city_air:
+                session.write_transaction(unique_id, a, shr_city_air.pop(random.randrange(len(shr_city_air))), "Airplane")
+            else: 
+                break
+        for b in shr_city:
+            if shr_city_train:
+                session.write_transaction(unique_id, b, shr_city_train.pop(random.randrange(len(shr_city_train))), "Train")
+            else: 
+                break
+
+        for c in azer_city:
+            if azer_city_air:
+                session.write_transaction(unique_id, c, azer_city_air.pop(random.randrange(len(azer_city_air))), "Airplane")
+            else: 
+                break
+        for d in azer_city:
+            if azer_city_train:
+                session.write_transaction(unique_id, d, azer_city_train.pop(random.randrange(len(azer_city_train))), "Train")
+            else: 
+                break
+
+    driver.close()
+
+def search_train(tx, transport):
+    for transport in tx.run("MATCH (a:City)-[b:CONNECT_BY {transport: $transport}]->(c:City)"
+                            "RETURN a,b,c", 
+                            transport=transport):
+        print("Из", transport.data()['c']['name'], "можно добраться поездом до", transport.data()['a']['name'])
+
+def print_train():
+    with driver.session() as session:
+        s = session.read_transaction(search_train, "Train")
+    driver.close()
+    return s
+    
+
+
+def main():
+    pegas()
+    biblio()
+    coral()
+    print_train()
+
+main()
